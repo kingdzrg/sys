a072ace8ac469704b53a5431a2e7424e742b9396
kingdzrg <58010386+kingdzrg@users.noreply.github.com>
2019-12-19 23:01:51 +0100

    Update System.js

--- System.js
+++ System.js
@@ -14,17 +14,6 @@ client.on('ready', () => {
    console.log(`----------------`);
 });
 
-var prefix = "1"; 
-client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`); }); client.on('ready', async() 
-=> { var server = "538407182736031744"; // ايدي السررفر var channel = "654037512838184974";//ايدي الروم 
-setInterval(()=>{ client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , 
-Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , 
-Dream , **') },305); }) client.on('message', message => { if (message.author.bot) return; if 
-(!message.content.startsWith(prefix)) return; let command = message.content.split(" ")[0]; command = 
-command.slice(prefix.length); let args = message.content.split(" ").slice(1); if (command == "say") { 
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR`\ليس 
-لديك صلاحيات**'); message.channel.sendMessage(args.join(" ")) message.delete() } }); 
-
 client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","・ToPs"));
     });
